156597466c24de1c679fe83f30946b3ab08c3a31
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-01-14 23:44:56 +0530

    gif added

---
+++ main.py
@@ -0,0 +1,29 @@
+import pytesseract
+import io 
+import requests
+from PIL import Image, ImageEnhance 
+import PIL.ImageOps
+
+
+img = Image.open('akio.gif')
+
+
+for frame in range(img.n_frames):
+
+    print('Frame: ', frame + 1)
+
+    img.seek(frame)
+    imgrgb = img.convert('RGB')
+
+    imgrgb = PIL.ImageOps.invert(imgrgb)
+
+    enchancer = ImageEnhance.Brightness(imgrgb)
+
+    imgrgb = enchancer.enhance(4.0)
+
+    imgrgb = imgrgb.convert('1')
+
+    text = pytesseract.image_to_string(imgrgb)
+
+    print(text)
+    print("------------------")
